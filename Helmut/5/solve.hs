61af349e9004d3c4e2f1d90934f827360d4e4506
Gitea <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-05 20:15:25 +0100

    wip

--- Helmut/5/solve.hs
+++ Helmut/5/solve.hs
@@ -1,8 +1,10 @@
+-- f xs l = 
 stacks txt = 
     let 
         blocks = reverse $ init $ takeWhile (/="") txt
         m = div  ((+1) $ length $ head blocks) 4
-    in  m
+        z0 = take m  (repeat []) :: [[Char]]
+     in z0
 
 main = do 
     txt <- fmap lines $ readFile "small.txt"
