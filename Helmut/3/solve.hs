7f6329d34c411e0d48ad823049505b0f5c71b390
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-06 13:36:39 +0100

    Aufgabe 3

---
+++ Helmut/3/solve.hs
@@ -0,0 +1,13 @@
+import Data.Char (ord, isLower)
+import Data.List (intersect)
+
+e c = if isLower c then 1 + ord c - ord 'a'
+                   else 27 + ord c - ord 'A'
+
+f r = let (a,b) = splitAt (div (length r) 2) r
+      in head $ map e $ intersect a b
+
+
+main = do 
+    fmap (sum . map f . lines) $ readFile "small.txt"
+    fmap (sum . map f . lines) $ readFile "input"
