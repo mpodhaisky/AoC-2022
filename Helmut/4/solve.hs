0b93abcebae5129a6533c2ea0d24669c103754dc
Gitea <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-06 21:23:01 +0100

    Teil 2 von 4

--- Helmut/4/solve.hs
+++ Helmut/4/solve.hs
@@ -5,10 +5,16 @@ g = map read . splitOn "-"
 
 contains [a,b] [c,d] = ((a <= c) && ( d <= b )) || (( c <= a ) && ( b <= d ))
 
-check line = let [p1,p2] = splitOn "," line
-             in contains (g p1) (g p2)
+overlaps1 [a,b] [c,d] = ((c <= a) && ( a <= d )) || (( c <= b ) && ( b <= d ))
+
+overlaps x y = (overlaps1 x y) || (overlaps1 y x)
+
+check f line = let [p1,p2] = splitOn "," line
+             in f (g p1) (g p2)
 main = do
     small <- fmap lines $ readFile "small.txt"
     big <- fmap lines $ readFile "big.txt"
-    print $ filter check small
-    print $ length $ filter check big
+    print $ filter (check contains) small
+    print $ filter (check overlaps) small
+    print $ length $ filter (check contains) big
+    print $ length $ filter (check overlaps) big
