4d91319e4d89db8e5e8ba3ea95985a19ab3f99a1
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-06 15:29:47 +0100

    mhm

---
+++ Helmut/2/solve.py
@@ -0,0 +1,14 @@
+# what is wrong?
+
+def f(a,b):
+    return b+1 + (6 if b>a else (3 if b == a else 0) )
+
+p = 0
+for l in open("input","r").readlines():
+    a, b = l.strip().split(" ")
+    a = ord(a)-ord('A')
+    b = ord(b)-ord('X')
+    p += f(a,b)
+    print(l, a, b, f(a,b))
+
+print(p)
