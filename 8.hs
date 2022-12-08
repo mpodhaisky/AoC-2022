bf1a00734f753fdced3eda3c13b5aea0b742b349
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2022-12-08 11:17:01 +0100

    eternal pain

--- 8.hs
+++ 8.hs
@@ -1,10 +1,9 @@
 readInt:: String -> Int
 readInt = read
 
-visible:: String -> [Bool]
-visible xs = (`fill` length xs).takeWhile (==True) . (True:).(zipWith (<) <*> tail) $ xs
 
-fill xs n = xs ++ [False | i<-[1..n-(length xs)]]
+visible:: String -> [Bool]
+visible xs= [and $ map (<(xs!!(i-1))) (take (i-1) xs) | i<-[1..(length xs)]]
 
 getter xs = filter (/=[]) (go xs 0 (length (head xs)))
     where
@@ -12,10 +11,23 @@ getter xs = filter (/=[]) (go xs 0 (length (head xs)))
         
 ree True = 1
 ree False = 0
+
+length' [] = 1
+length' xs = length xs
+
+prod xs n = (length' (go' (reverse (take n xs)) (xs!!n)))*(length' (go' (drop (n+1) xs) (xs!!n)))
+    where
+        go' [] _ = []
+        go' (x:xs) n = if x<n then (x:(go' xs n)) else [x] 
+
+viewDistance::Int->Int->[String]->[String]->Int
+viewDistance x y xs ys = prod (xs!!y) x * prod (ys!!x) y
+
 main = do
     input <- readFile "8.txt"
-    print .sum $map (sum.map (ree)) (zipWith (zipWith (||)) (zipWith (zipWith (||)) (map visible (lines input)) (map (reverse.visible.reverse) (lines input))) (zipWith (zipWith (||)) (getter (map visible (getter (lines input)))) (getter (map (reverse.visible.reverse) (getter(lines input))))))
-    print $ lines input
-    print $ (zipWith (zipWith (||)) (map visible (lines input)) (map (reverse.visible.reverse) (lines input)))
-    print $ getter (lines input)
-    print $ (zipWith (zipWith (||)) (getter (map visible (getter (lines input)))) (getter (map (reverse.visible.reverse) (getter(lines input)))))
+    let ree1 = (zipWith (zipWith (||)) (map visible (lines input)) (map (reverse.visible.reverse) (lines input)))
+    let ree2 = getter (zipWith (zipWith (||)) ((map visible (getter (lines input)))) ( (map (reverse.visible.reverse) (getter(lines input)))))
+    let rows = lines input
+    let cols = getter (lines input)
+    print $ maximum [viewDistance x y rows cols| x<-[1..98],y<-[1..98]]
+
