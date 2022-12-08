b877c8d262541d8833139e3c8b2b33d9584997d2
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2022-12-08 13:17:31 +0100

    cleanup day 8

--- 8.hs
+++ 8.hs
@@ -9,8 +9,8 @@ getter xs = filter (/=[]) (go xs 0 (length (head xs)))
     where
         go xs i n = if i==n then [[]] else (map (!!i) xs):go xs (i+1) n
         
-ree True = 1
-ree False = 0
+toNum True = 1
+toNum False = 0
 
 length' [] = 1
 length' xs = length xs
@@ -25,9 +25,22 @@ viewDistance x y xs ys = prod (xs!!y) x * prod (ys!!x) y
 
 main = do
     input <- readFile "8.txt"
+    tinput <- readFile "8test.txt"
+    
+    let tree1 = (zipWith (zipWith (||)) (map visible (lines tinput)) (map (reverse.visible.reverse) (lines tinput)))
+    let tree2 = getter (zipWith (zipWith (||)) ((map visible (getter (lines tinput)))) ( (map (reverse.visible.reverse) (getter(lines tinput)))))
+    
     let ree1 = (zipWith (zipWith (||)) (map visible (lines input)) (map (reverse.visible.reverse) (lines input)))
     let ree2 = getter (zipWith (zipWith (||)) ((map visible (getter (lines input)))) ( (map (reverse.visible.reverse) (getter(lines input)))))
+    --solution part one
+    print . sum.map (sum.map toNum)$ zipWith (zipWith (||)) tree1 tree2
+    print . sum.map (sum.map toNum)$ zipWith (zipWith (||)) ree1 ree2
+    
     let rows = lines input
     let cols = getter (lines input)
-    print $ maximum [viewDistance x y rows cols| x<-[1..98],y<-[1..98]]
+    let trows = lines tinput
+    let tcols = getter (lines tinput)
 
+    --solution part two
+    print $ maximum [viewDistance x y trows tcols| x<-[1..3],y<-[1..3]]
+    print $ maximum [viewDistance x y rows cols| x<-[1..98],y<-[1..98]]
