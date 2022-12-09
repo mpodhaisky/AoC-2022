b68dbd0bec7fc1fc35edc8dee17c189c902487f6
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-09 13:00:14 +0100

    Diskussionsvorschlag

--- Helmut/9/solve.hs
+++ Helmut/9/solve.hs
@@ -1,5 +1,5 @@
 import Data.List
--- import Data.Set -- später anstelle von nub
+import Data.Set (fromList) -- besser als nub, braucht aber Ord
 
 f :: Int -> Int -> Int
 f x y = y + (signum (x-y))
@@ -11,16 +11,14 @@ follow (a,b) (c,d) =
       then (c,d)
       else (f a c, f b d)
 
-move1 ((x,y):snake) (dx,dy) = let
-   newhead = (x+dx, y+dy)
-   in scanl follow newhead snake
+move1 ((x,y):snake) (dx,dy) = scanl follow (x+dx, y+dy) snake
 
 type Pos = (Int, Int) 
 type Loc = ([Pos], Pos)
 
 parse :: String -> [Pos]
 parse (move:n) =  let dir = case move of { 
-     'L' -> (-1,0) ; 'R' -> (1,0) ; 'U' -> (0,1) ; 'D' -> (0,1) }
+     'L' -> (-1,0) ; 'R' -> (1,0) ; 'U' -> (0,1) ; 'D' -> (0,-1) }
   in replicate (read n) dir
 
 kurzeSchlange = replicate 2 (0,0)
@@ -31,3 +29,13 @@ test2 = move1 [(0,0),(0,0)] (1,0)
 test3 = foldl move1 kurzeSchlange test1
 test4 = foldl move1 langeSchlange test1
 
+trace schlange moves  = length $ fromList $ map last $ scanl move1 schlange moves
+
+main = do
+   small <- readFile "small.txt"
+   medium <- readFile "medium.txt"
+   large <- readFile "input"
+   print $ trace kurzeSchlange $ concatMap parse (lines small)
+   print $ trace langeSchlange $ concatMap parse (lines medium)
+   print $ trace langeSchlange $ concatMap parse (lines large)
+   
