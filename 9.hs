fca405092b9b9fb0f9f93ffb437e2ac4ad9daf70
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2022-12-09 15:34:22 +0100

    day 9 part 2

--- 9.hs
+++ 9.hs
@@ -15,16 +15,32 @@ move' (xs,(a,b)) (_,(c,d))
     | abs (c-a)==0 && abs (d-b)==2 = ((a,b+ (d-b)`div`2):xs,(a,b+ (d-b)`div`2))
     | abs (c-a)==2 && abs (d-b)==1 = ((a+ (c-a)`div`2,d):xs,(a+ (c-a)`div`2,d))
     | abs (c-a)==1 && abs (d-b)==2 = ((c,b+ (d-b)`div`2):xs,(c,b+ (d-b)`div`2))
+    | abs (c-a)==2 && abs (d-b)==2 = ((a+ (c-a)`div`2,b+ (d-b)`div`2):xs,(a+ (c-a)`div`2,b+ (d-b)`div`2))
     | otherwise = (xs, (a,b))
 
 pack (a,b) = ([],(a,b)) ::Loc
 spew (xs,_) = length (nub xs)
+unpack (xs, _) = xs
 
 readInt:: String -> Int
 readInt = read
 
+solver = reverse.unpack .last.scanl move' ([(0,0)],(0,0)) .map pack
+
 main = do
     input <- readFile "9test.txt"
     input1 <- readFile "9.txt"
-    print .spew .last.scanl move' ([(0,0)],(0,0)) .map pack.scanl move (0,0).concat.map (parse.words). lines $ input
-    print .spew .last.scanl move' ([(0,0)],(0,0)) .map pack.scanl move (0,0).concat.map (parse.words). lines $ input1
+    input2 <- readFile "9test2.txt"
+    let testmoves = solver .scanl move (0,0).concat.map (parse.words). lines $ input
+    let moves1 = solver .scanl move (0,0).concat.map (parse.words). lines $ input1
+    let moves2 = solver moves1
+    let moves3 = solver moves2
+    let moves4 = solver moves3
+    let moves5 = solver moves4
+    let moves6 = solver moves5
+    let moves7 = solver moves6
+    let moves8 = solver moves7
+    let moves9 = solver moves8
+    let moves10 = solver moves9
+    print .length.nub$moves9
+    
