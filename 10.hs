b6c93c66c2a1158189355ad7dac31185491abe94
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-10 09:48:28 +0100

    substitute prodT by uncurry (*)

--- 10.hs
+++ 10.hs
@@ -3,8 +3,6 @@ parse xs= [(0,1),((read (drop 5 xs)),1)]
 
 addT (a,b) (c,d) = (a+c,b+d)
 
-prodT (a,b) = a*b
-
 filter40 (_,n)= (n-20)`mod`40==0
 
 eval (x,t) = if abs (t`mod`40-x) <=1 then "#" else "." 
@@ -14,5 +12,5 @@ breakLines n = unlines . map (take n) . takeWhile (/=[]) . iterate (drop n)
 
 main = do
     input <- readFile "10.txt"
-    print .sum.map prodT.filter filter40.scanl addT (1,1).concatMap parse.lines $ input
+    print .sum.map (uncurry (*)).filter filter40.scanl addT (1,1).concatMap parse.lines $ input
     putStrLn . breakLines 40. concatMap eval.scanl addT (1,0).concatMap parse.lines $ input
