aca253894ef458c77d99312d7397ae18c592661f
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2022-12-10 08:53:41 +0100

    adjusted index

--- 10.hs
+++ 10.hs
@@ -3,20 +3,13 @@ parse xs= [(0,1),((read (drop 5 xs)),1)]
 
 addT (a,b) (c,d) = (a+c,b+d)
 
-prodT (a,b) = a*(b+1)
+prodT (a,b) = a*b
 
-filter40 (_,n)= (n-19)`mod`40==0
+filter40 (_,n)= (n-20)`mod`40==0
 
 eval (x,t) = if abs (t`mod`40-x) <=1 then "#" else "." 
 
 main = do
     input <- readFile "10.txt"
-    print .sum.map prodT.filter filter40.scanl addT (1,0).concatMap parse.lines $ input
+    print .sum.map prodT.filter filter40.scanl addT (1,1).concatMap parse.lines $ input
     print.concatMap eval.scanl addT (1,0).concatMap parse.lines $ input
-
--- ###..#.....##..####.#..#..##..####..##..
--- #..#.#....#..#.#....#.#..#..#....#.#..#.
--- #..#.#....#....###..##...#..#...#..#....
--- ###..#....#.##.#....#.#..####..#...#.##.
--- #....#....#..#.#....#.#..#..#.#....#..#.
--- #....####..###.#....#..#.#..#.####..###..
