bcac086129b5bf5f87fddd22eceddd608a48b2e6
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2022-12-12 14:25:09 +0100

    working on dijkstra's

--- 12.hs
+++ 12.hs
@@ -1,32 +1,36 @@
-readInt:: String -> Int
-readInt = read
+type Knot = (Int,Int,Int)
+type Edge = (Knot,[Knot])
+type Path = (Knot,Knot)
 
 packer c = (fromEnum c-97,1,1)
 
 addx (_,a,_) (c,x,y) = (c,x+a,y)
 addy (_,_,a) (c,x,y) = (c,x,y+a)
 
-startEnd (-14,_,_) = True
-startEnd (-28,_,_) = True
-startEnd _ = False
+isStart (-14,_,_) = True
+isStart _ = False
+isEnd (-28,_,_) = True
+isEnd _ = False
 
 setn n (_,x,y) = (n,x,y)
 
-getChildren (n,x,y) (m,a,b) = abs (x-a)<=1 && abs (y-b)<=1 && abs (n-m)<=1 && not (x==a&&y==b)
+isChildOf (n,x,y) (m,a,b) = abs (x-a)<=1 && abs (y-b)<=1 && abs (n-m)<=1 && not (x==a&&y==b)
 
-remove [] ys = ys
-remove (x:xs) ys = remove xs (filter (/=x) ys)
+childrenOf knot knots = (knot,filter (isChildOf knot) knots)
 
-dfs:: [(Int,Int,Int)]->(Int,Int,Int) ->(Int,Int,Int) ->Int
-dfs heightmap endpoint r@(n,x,y) = if (children/=[]) then 1+minimum (map (dfs (filter (/=r) (remove children heightmap)) endpoint) children) else if r==endpoint then 1 else 1000000
-    where
-        children = filter (getChildren r) heightmap
+makePath a b = (a,b)
+
+dijkstra::Edge->[Edge]->[Path]->[Path]
+dijkstra (from, to) edges paths = paths++newPaths
+    where newPaths =(map (makePath from) to)
 
 main = do
     input <- readFile "12.txt"
-    let heightmap = concat. scanl1 (zipWith addy).map (scanl1 addx.map packer). lines $ input
-    let start = setn 0.head $ filter (startEnd) heightmap
-    let end = setn 25.last $ filter (startEnd) heightmap
-    print start
-    print end
-    print $ dfs heightmap end start
+    let knots = concat. scanl1 (zipWith addy).map (scanl1 addx.map packer). lines $ input
+    let start = setn 0.head $ filter isStart knots
+    let end = setn 26 .head $ filter isEnd knots
+    let knots' = start:end:(filter (not .isEnd).filter (not .isStart) $knots)
+    let edges =map (`childrenOf` knots') knots'
+
+    print edges
+    print $ dijkstra (head edges) edges []
