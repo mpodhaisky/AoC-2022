7f0d43a116068162fe7c50d4e7dad05b746c1021
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-13 12:13:59 +0100

    part 1

--- Helmut/13/solve.hs
+++ Helmut/13/solve.hs
@@ -1,13 +1,13 @@
 import Data.List (intercalate) 
 import Text.Read (readMaybe)
 
-data T = L Int | K [T] deriving (Eq)
+data T = L Int | K [T] deriving (Eq, Read)
 
-t1 = K [L 1, L 1, L 3, L 1, L 1]
-t2 = K [L 1, L 1, L 5, L 1, L 1]
+t1 = read "K [L 1, L 1, L 3, L 1, L 1]" :: T
+t2 = read "K [L 1, L 1, L 5, L 1, L 1]" :: T
 
-t3 = K [ K [L 1], K [L 2, L 3, L 4]]
-t4 = K [ K [L 1], L 4]
+t3 = read "K [ K [L 1], K [L 2, L 3, L 4]]" :: T
+t4 = read "K[ K[L 1], L 4]" :: T
 
 instance Show T where
   show (L a) = show a
@@ -30,3 +30,15 @@ parseL s = fmap L (readMaybe s :: Maybe Int)
 
 
 test1 = t3 <= t4
+
+pairs :: [String] -> [[String]]
+pairs = map (take 2) . takeWhile (/=[]) . iterate (drop 3 ) 
+
+check :: [String] -> Bool
+check [a,b] = ((read a)::T) <= (read b)
+
+-- vim %s/\(\d\+\)/L \1/g
+--
+main = do
+    l <- fmap lines $ readFile "input1.txt"
+    print $ sum $ map snd $ filter fst $ zip (map check $ pairs l ) [1..]
