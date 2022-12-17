e32a1b70da754cf63bf4108ea98a84879c7590c7
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-17 20:58:48 +0100

    works only for dönne

--- Helmut/17/tetris.py
+++ Helmut/17/tetris.py
@@ -1,7 +1,6 @@
 def flow_():
     r = open("input.txt","tr").readline()
     pat = r.strip()
-    print(len(pat))
     epoch = 0
     while True:
         yield pat[epoch % len(pat)]
@@ -29,15 +28,19 @@ def move(s, p):
 def top(S):
     return 0 if len(S) == 0 else max(y for (_,y) in S)
 
-def bildchen(S, p):
-    ny = 15
-    topy = top(S.union(p))
-    B = [['.']*7 for i in range(ny)]
-    for e, c in zip((S,p),('#','@')):
-        for (x,y) in e:
+def fingerprint(S):
+    ny = 50
+    topy = top(S)
+    return set((x,y-topy+ny) for (x,y) in S if topy-y < ny)
+
+def bildchen(S):
+    ny = 50
+    topy = top(S)
+    B = [['.']*ny for i in range(7)]
+    for (x,y) in S:
             if topy-y<ny:
-                B[topy-y][x] = c
-    print("\n".join(["".join(B[i]) for i in range(ny)]))
+                B[x][topy-y] = '#'
+    return ("\n".join(["".join(B[i]) for i in range(7)]))
 
 def collides(p, S):
     links = min(x for (x,_) in p)<0
@@ -46,7 +49,8 @@ def collides(p, S):
 
 def addPiece(S):
     p = nextp.__next__()
-    dy = top(S) + 4
+    topy = top(S)
+    dy = topy + 4
     p = set((x+2,y+dy) for (x,y) in p)
 
     for c in flow:
@@ -64,10 +68,41 @@ def addPiece(S):
         if min(y for (_,y) in p) == 0:
             break
     S.update(p)
+    S = set((x,y) for (x,y) in S if topy - y<100)
+    return S 
+
+def dectectCycleLength(S):
+    for k in range(1,20):
+        z0 = bildchen(S)
+        top0 = top(S)
+        print("checke nun auf Zyklus der Länge <= ", 2**k)
+        for t in range(2**k):
+            S = addPiece(S)
+            z1 = bildchen(S)
+            if z0 == z1:
+                d = top(S)-top0
+                print("Aha", t, d)
+                return t+1, d 
 
 S = set()
 
-for _ in range(2022):
-    addPiece(S)
+T, d = dectectCycleLength(S)
 
-print(top(S)+1)
+t0 = 5000
+tt = 10**12
+
+# nextp = nextp_()
+# flow = flow_()
+# S = set()
+# for _ in range(tt):
+#     S = addPiece(S)
+# print(top(S))
+
+nextp = nextp_()
+flow = flow_()
+S = set()
+for _ in range(t0+(tt-t0)%T):
+    S = addPiece(S)
+ss = ((tt - t0) // T)
+speedup = d * ss  
+print(top(S) + speedup+1)
