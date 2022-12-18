e702cecce99bf3cabefdfbf95bcb6cf50f0f2bc0
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-18 07:21:16 +0100

    dönne

--- Helmut/18/solve.jl
+++ Helmut/18/solve.jl
@@ -6,19 +6,31 @@ function hexagon(xyz)
     [(x+1,y,z),(x-1,y,z),(x,y+1,z),(x,y-1,z),(x,y,z+1),(x,y,z-1)]
 end
 
-function isinner(face)
+function incidence(face)
     c, d = face
-    (c in cubes) && (d in cubes)
+    (c in cubes) + (d in cubes)
 end 
 
 
+#= 
+woelf ) (u, x, y, z)
+    [Set([(x,y+1,z),(u,y+1,z)]),Set([(x,y-1,z),(u,y-1,z)]),
+        Set([(x,y,z+1),(u,y,z+1)]), Set([(x,y,z-1),(u,y,z-1)]),
+        Set([(x,y,z), (x,y+1,z)]),Set([(x,y,z), (x,y-1,z)]),
+        Set([(x,y,z), (x,y,z-1)]),Set([(x,y,z), (x,y,z+1)]),
+        Set([(u,y,z), (u,y,z-1)]),Set([(u,y,z), (u,y,z+1)]),
+        Set([(u,y,z), (u,y,z-1)]),Set([(u,y,z), (u,y,z+1)]),
+        ] 
+end
+#=
+
 faces = Set([Set([xyz, uvw]) for xyz in cubes for uvw in hexagon(xyz)])
 
-outside = [f for f in faces if !isinner(f)]
+outside = [f for f in faces if incidence(f) == 1]
 
 (x0,y0,z0) = minimum(cubes)
 f0 = Set([(x0,y0,z0),((x0-1), y0, z0)])
-seen = Set(f0)
+
 
 # part 1:
 println(length(outside))
