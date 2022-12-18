dec1ff78a79118e7ecfee845ea3d6da69036d05b
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-18 06:54:05 +0100

    part1 -- hübescher

--- Helmut/18/solve.jl
+++ Helmut/18/solve.jl
@@ -1,12 +1,24 @@
-fn = "small.txt"
+fn = "input.txt"
 cubes = Set(map(l->tuple(parse.(Int,split(l,','))...), readlines(open(fn,"r"))))
 
-function adj(c)
-    (x,y,z) = c
-    filter(xyz->xyz in cubes, [(x+1,y,z),(x-1,y,z),(x,y+1,z),(x,y-1,z),(x,y,z+1),(x,y,z-1)])
+function hexagon(xyz)
+    x,y,z = xyz
+    [(x+1,y,z),(x-1,y,z),(x,y+1,z),(x,y-1,z),(x,y,z+1),(x,y,z-1)]
 end
 
-edges = [(c,d) for c in cubes for d in adj(c)]
+function isinner(face)
+    c, d = face
+    (c in cubes) && (d in cubes)
+end 
 
-println(6*length(cubes)-length(edges))
 
+faces = Set([Set([xyz, uvw]) for xyz in cubes for uvw in hexagon(xyz)])
+
+outside = [f for f in faces if !isinner(f)]
+
+(x0,y0,z0) = minimum(cubes)
+f0 = Set([(x0,y0,z0),((x0-1), y0, z0)])
+seen = Set(f0)
+
+# part 1:
+println(length(outside))
