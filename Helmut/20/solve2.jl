6d3058fbc09ce511b4f61482bac4a9b801920787
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-20 19:47:53 +0100

    part 1

--- Helmut/20/solve2.jl
+++ Helmut/20/solve2.jl
@@ -1,5 +1,22 @@
+
+function fd(x,xs)
+    for (i,y) in enumerate(xs)
+        if y == x
+            return i
+        end
+    end
+end
+
+function fd0(xs)
+    for (i,y) in enumerate(xs)
+        if y[2] == 0
+            return i
+        end
+    end
+end
+
 function right(x, k, xs)
-    i = indexin(x,xs)[1]
+    i = fd(x,xs)
     for _ in 1:k
         ip = mod(i, length(xs))+1
         xs[i], xs[ip] = xs[ip], xs[i]
@@ -8,7 +25,7 @@ function right(x, k, xs)
 end
 
 function left(x, k, xs)
-    i = indexin(x,xs)[1]
+    i = fd(x,xs)
     for _ in 1:k
         im = mod(i-2, length(xs))+1
         xs[i], xs[im] = xs[im], xs[i]
@@ -17,7 +34,7 @@ function left(x, k, xs)
 end
 
 function find0(xs, k)
-    i = indexin(0, xs)[1]
+    i = fd0(xs)
     for _ in 1:k
         i = mod(i, length(xs))+1
     end
@@ -25,17 +42,17 @@ function find0(xs, k)
 end
 
 function part1()
-    xs = parse.(Int, readlines(open("input.txt")))
-    xs = [1, 2, -3, 3, -2, 0, 4]
+    xs = collect(enumerate(parse.(Int, readlines(open("input.txt")))))
+    #xs = collect(enumerate([1, 2, -3, 3, -2, 0, 4]))
     ys = copy(xs)
     for x in ys
-        if x > 0
-            right(x, x, xs)
-        elseif x<0
-            left(x,-x, xs)
+        if x[2] > 0
+            right(x, x[2], xs)
+        elseif x[2]<0
+            left(x,-x[2], xs)
         end
     end
-    lsg = [find0(xs, 1000),find0(xs,2000),find0(xs,3000)]
+    lsg = [find0(xs, 1000)[2],find0(xs,2000)[2],find0(xs,3000)[2]]
     println(lsg, sum(lsg))
     display(xs)
 end
