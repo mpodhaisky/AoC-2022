5ea17b623f2618d8eb13361b187a36ad143628a4
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2022-12-20 09:47:24 +0100

    day 13 part 1

--- 13.hs
+++ 13.hs
@@ -1,16 +1,31 @@
-import Data.Tree
+data Exp = Bracket [Exp] | Num Int deriving (Show, Read)
 
-parse '[' = "Node {rootLabel = Nothing, subForest = ["
-parse ',' = ","
-parse ']' = "]}"
-parse n = "Node {rootLabel = Just "++[n]++",subForest = []}"
-
+parse:: String->String
+parse ('[':xs) = "Bracket ["++parse xs
+parse (',':xs) = ","++parse xs
+parse (']':xs) = "]"++parse xs
+parse [] = []
+parse (xs) = "Num "++(takeWhile condition xs)++parse (dropWhile condition xs)
+    where
+        condition a = or $ map (==(a:"")) (map show (take 10 (iterate ((+1)) 0)))
 breakLines n = map (take n) . takeWhile (/=[]) . iterate (drop n)
 
-readTree:: String->Tree (Maybe Int)
+readTree:: String->Exp
 readTree = read
 
+comp (Num l) (Num r) = if l==r then 0 else if l<r then 1 else 2
+comp (Num l) r = comp (Bracket [Num l]) r 
+comp l (Num r) = comp l (Bracket [Num r])
+comp (Bracket (l:ls)) (Bracket (r:rs)) = if comp l r ==0 then comp (Bracket ls) (Bracket rs) else comp l r
+comp (Bracket []) (Bracket []) = 0
+comp (Bracket []) _ = 1
+comp _ _ = 2
+
+comp' [a , b] = comp a b
+
+f a b = if b == 1 then a else 0
+
 main = do
-    input <- readFile "13test.txt"
+    input <- readFile "13.txt"
     
-    print.breakLines 2.map (readTree.concatMap parse).filter (/="").lines $ input
+    print.sum.zipWith f (iterate (+1) 1).map (comp'.map(readTree.parse)).breakLines 2.filter (/="").lines $ input
