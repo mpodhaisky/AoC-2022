e0ddd79b71279bc3a3079fa3b4123e0420ae7d23
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-21 05:19:59 +0100

    polish

--- Helmut/20/solve2.jl
+++ Helmut/20/solve2.jl
@@ -8,6 +8,22 @@ function right(x, k, xs)
     end
 end
 
+function fd(x,xs)
+    for (i,y) in enumerate(xs)
+        if y == x
+            return i
+        end
+    end
+end
+
+function fd0(xs)
+    for (i,y) in enumerate(xs)
+        if y[2] == 0
+            return i
+        end
+    end
+end
+
 
 function find0(xs, k)
     i = fd0(xs)
@@ -17,32 +33,20 @@ function find0(xs, k)
     xs[i]
 end
 
-function part1()
-    xs = collect(enumerate(parse.(Int, readlines(open("input.txt")))))
-    #xs = collect(enumerate([1, 2, -3, 3, -2, 0, 4]))
+function solve(part)
+    fac, nrepeat = Dict(1=>(1,1), 2=>(811589153,10))[part]
+    xs = collect(enumerate(fac*parse.(Int, readlines(open("input.txt")))))
+    #xs = collect(enumerate(fac*[1, 2, -3, 3, -2, 0, 4]))
     ys = copy(xs)
     n = length(xs)
-    for x in ys
-        right(x, mod(x[2], n-1), xs)
-    end
-    lsg = [find0(xs, 1000)[2],find0(xs,2000)[2],find0(xs,3000)[2]]
-    println(lsg, sum(lsg))
-end
-
-function part2()
-    xs = collect(enumerate(811589153 * [1, 2, -3, 3, -2, 0, 4]))
-    xs = collect(enumerate(811589153 * parse.(Int, readlines(open("input.txt")))))
-    ys = copy(xs)
-    n = length(xs)
-    for _ in 1:10
+    for _ in 1:nrepeat
         for x in ys
             right(x, mod(x[2], n-1), xs)
         end
-        end
-    lsg = [find0(xs, 1000)[2],find0(xs,2000)[2],find0(xs,3000)[2]]
-    println(lsg, sum(lsg))
+    end
+    lsg = [find0(xs, k*1000)[2] for k in 1:3]
+    println(lsg, " ", sum(lsg))
 end
 
-# -3886 wrong
-part1()
-part2()
+solve(1)
+solve(2)
