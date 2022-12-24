202b44fb36f3c2c785825054e1185b15736cc564
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-24 18:32:43 +0100

    tend

--- Helmut/19/solve2.jl
+++ Helmut/19/solve2.jl
@@ -1,21 +1,22 @@
 using LinearAlgebra
 using Printf 
+tend = 24
 
-S0 = fill(0, 24, 4)
-S1 = fill(0, 24, 4)
-S0[:,1] .= 0:23
+S0 = fill(0, tend, 4)
+S1 = fill(0, tend, 4)
+S0[:,1] .= 0:tend-1
 S1[:,1] .= 1
 
-M = fill(0, 24, 24)
-K = fill(0, 24, 24)
-K1 = fill(0, 24, 24)
-for i in 3:24
+M = fill(0, tend,tend)
+K = fill(0, tend, tend)
+K1 = fill(0, tend, tend)
+for i in 3:tend
     M[i,1:i-2] .= i-2:-1:1
 end
-for i in 1:24
+for i in 1:tend
     K[i,1:i] .= 1
 end
-for i in 2:24
+for i in 2:tend
     K1[i,1:i-1] .= 1
 end
 
@@ -33,26 +34,14 @@ function b2c(ll)
     return C
 end
 
-function readInput()
-    ll = readlines(open("input.txt"))
+function readInput(fn)
+    ll = readlines(open(fn))
 end
 
-
-
-function demo()
-    X = fill(0, 24, 4)
-    for (i,s) in enumerate(readlines(open("muster.txt")))
-        if length(s)>0
-            X[i, parse(Int, s)+1] = 1
-        end
-    end
-    X
-end
 
 function cond(C, X)
    A = M*X-K*X*C+S0
    R = A+K1*X + S1
-  # r = A[end,:]+vcat(ones(Int, 23),0)*X + [1,0,0,0]
    (A, R)
 end
 
@@ -64,22 +53,22 @@ end
 
 function ampl(C)
     open("job.ampl", "w") do out
-        write(out, "var x {t in 1..24, i in 1..4} binary;\n\n")
+        write(out, "var x {t in 1.." * string(tend) * ", i in 1..4} binary;\n\n")
         U = Dict()
         Z = String[]
-        X0 = zeros(Int, 24, 4)
+        X0 = zeros(Int, tend, 4)
         A0, R0 = cond(C, X0)
         r0 = R0[end,4]
         if r0!=0
             push!(Z, string(r0))
         end
-        for ii in 1:24
+        for ii in 1:tend
             for jj in 1:4
                 U[ii,jj] = [string(A0[ii,jj])]
             end
         end
 
-        for i in 1:24
+        for i in 1:tend
             for j in 1:4
                 A1, R1 = cond(C, Xij(i,j))
                 A = A1-A0
@@ -88,7 +77,7 @@ function ampl(C)
                 if r != 0
                     push!(Z, @sprintf "%d * x[%d, %d]" r i j)
                 end
-                for ii in 1:24
+                for ii in 1:tend
                     for jj in 1:4
                         if A[ii,jj] != 0 
                             push!(U[ii,jj], @sprintf "%d * x[%d, %d]" A[ii,jj] i j )
@@ -98,7 +87,7 @@ function ampl(C)
             end
         end
         write(out, "maximize z: " * join(Z, " + ")*" ;\n\n")
-        for ii in 1:24
+        for ii in 1:tend
             for jj in 1:4
                 write(out, @sprintf "subject to u%d_%d: %s" ii jj join(U[ii,jj]," + ")*">=0 ;\n")
             end
@@ -107,7 +96,7 @@ function ampl(C)
         write(out, 
         """
         solve; 
-        for {i in 1..24} { for {j in 1..4} {printf "%1d ",x[i,j];}printf "\\n";} 
+        for {i in 1.."""*string(tend)*"""} { for {j in 1..4} {printf "%1d ",x[i,j];}printf "\\n";} 
         printf "\\n\\nz = %d\\n", z;\n
         """)
     end
@@ -115,17 +104,15 @@ end
 
 
 function main()
-    cs = map(b2c, readInput())
+    cs = map(b2c, readInput("input2.txt"))
     total = 0
     for (i, C) in enumerate(cs)
         ampl(C)
         output = readchomp(`glpsol "--math" "job.ampl"`)
         res = split(output,"\n")[end-1]
-        println(res, " ", total)
+        println(res)
         total += parse(Int, res[4:end])*i
     end
     println(total)
 end
 main()
-
-
