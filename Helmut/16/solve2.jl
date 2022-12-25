8585e98c305ec2083946a717da79a4445a8fec91
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-25 09:12:58 +0100

    refactor part1

--- Helmut/16/solve2.jl
+++ Helmut/16/solve2.jl
@@ -16,7 +16,13 @@ function getinput(fn)
     D = Dict{Tuple{String,String}, Integer}()
     for e in keys(E)
         for f in keys(E)
-            D[e,f] = if f in E[e]; 1 else infty; end 
+            if e == f 
+                D[e,f] = 0
+            elseif f in E[e]
+                D[e,f] = 1
+            else
+                D[e,f] = infty
+            end 
         end
     end
     for v in keys(E)
@@ -26,14 +32,16 @@ function getinput(fn)
             end
         end
     end
-    V = [v for v in keys(R) if R[v]>0 || v == "AA"]
-    D1 =  Dict{Tuple{String,String}, Integer}()
-    for v in V
-        for w in V
-            D1[v,w] = D[v,w] + 1
-        end
+    V = Dict(1=>"AA")
+    for (l, v) in enumerate([v for v in keys(R) if R[v]>0])
+        V[l+1] = v 
+    end 
+    n = length(V)
+    D1 =  zeros(Int, n,n)
+    for i in 1:n, j in 1:n
+        D1[i,j] = D[V[i],V[j]]+1
     end
-    return V, D1, R
+    return D1, [R[V[i]] for i in 1:n]
 end
 
 function backtrack(v, seen, t, V, D, R, pr, bestSoFar)
@@ -57,7 +65,51 @@ function backtrack(v, seen, t, V, D, R, pr, bestSoFar)
     end
 end 
 
+function pfade(omega, tfinal)
+    set15 = Set()
+    q = [((1,),0)]
+    while length(q)>0
+        q = add1(q, omega, tfinal)
+        set15 = union(set15, q)
+    end
+    set15
+end
 
-V, D , R = getinput("input.txt")
+function w(v)
+    z = 0
+    t = 0
+    dr = 0
+    for i in 1:length(v)-1
+        dt = D[v[i],v[i+1]]
+        t += D[v[i],v[i+1]]
+        dr += R[v[i+1]]
+        z += (30-t)*R[v[i+1]]
+    end
+    (z,t,dr)
+end
 
-backtrack("AA", Set(), 0, V, D, R, 0, 0)
+function add1(pp, omega, tfinal)
+    r = []
+    for p in pp
+        yy,t = p
+        y = yy[end]
+        for x in setdiff(omega, yy)
+            d1 = D[y,x]
+            tt = t + d1
+            if tt <= tfinal
+                push!(r, (tuple(vcat(yy...,x)...), tt))
+            end
+        end
+    end
+    r
+end
+
+function part1()
+    omega = collect(2:15)
+    println(maximum([w(x)[1] for (x,t) in pfade(omega, 30)]))
+end
+
+D, R = getinput("input.txt")
+part1()
+
+# backtrack("AA", Set(), 0, V, D, R, 0, 0)
