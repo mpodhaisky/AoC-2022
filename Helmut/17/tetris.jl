da554db14306a279f5a9c8d3ee8ca5683f4dc709
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-26 20:50:04 +0100

    part 2 tetris

--- Helmut/17/tetris.jl
+++ Helmut/17/tetris.jl
@@ -6,7 +6,22 @@ function readPieces()
     Set([(0,0),(1,0),(1,1),(0,1)])]
 end
 
+function fingerprint(board)
+    nn = top(board)
+    M = fill('.',7,80)
+    for j in 1:80
+        for i in 1:7
+            if (i-1, nn-j) in board 
+                M[i,j] = '#'
+            end
+        end
+    end
+    join([join(M[i,:]) for i in 1:7],'\n')
+end
 
+function top(board)
+    maximum([y for (x,y) in board])
+end
 
 function tetris(flow, P, npieces)
     flow = Iterators.Stateful(cycle(flow))
@@ -15,6 +30,8 @@ function tetris(flow, P, npieces)
     for i in 0:6
         push!(board, (i,-1))
     end
+    fp = ""
+    top1 = 0
     for k = 1 : npieces
         y0 = maximum([y for (x,y) in board])+4
         x0 = 2
@@ -42,8 +59,17 @@ function tetris(flow, P, npieces)
                 break
             end    
         end       
+        if k == 3000 
+            fp = fingerprint(board)
+            top1 = top(board)
+        elseif k > 3000
+            if fp == fingerprint(board)
+                println("Zyklus ", k-3000)
+                println("dH = ", top(board)-top1)
+            end
+        end
     end
-    maximum([y for (x,y) in board])+1
+    board
 end
 
 fn = "input.txt"
@@ -51,5 +77,22 @@ flow = strip(read(open(fn), String))
 
 P = readPieces()
 
-tetris(flow, P, 2022)
+println("part1=", 1+ top(tetris(flow, P, 2022)))
+
+tetris(flow, P, 5000)
+
+big = 1000000000000
+
+T = 1745
+dH = 2750
+
+# ref1 = tetris(flow, P, big) |> top
+b1 = 3000 + mod(big-3000, T)
+b = tetris(flow, P, b1) |> top
+q = div(big-b1, T)
+ref = b + q * dH 
+
+println(ref+1)
+
+# 1576834862364 too high
 
