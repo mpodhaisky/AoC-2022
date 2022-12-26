467693f926f0f3da2f9e2da26892635b10747e84
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-26 19:57:54 +0100

    wip

--- Helmut/17/tetris.jl
+++ Helmut/17/tetris.jl
@@ -7,7 +7,7 @@ function readPieces()
         for (i,l) in enumerate(split(bl,"\n"))
             for (j,c) in enumerate(reverse(l))
                 if c == '#'
-                    push!(s, (i-1,j-1))
+                    push!(s, (j-1,i-1))
                 end
             end
         end
@@ -16,16 +16,62 @@ function readPieces()
     P
 end
 
-function tetris(flow, P, k)
-    fl = cycle(flow)
-    pp = cycle(P)
+
+
+function tetris(flow, P, npieces)
+    flow = Iterators.Stateful(cycle(flow))
+    pp = Iterators.Stateful(cycle(P))
+    board = Set{Tuple{Int,Int}}()
+    for i in 0:6
+        push!(board, (i,-1))
+    end
+    for k = 1 : npieces
+        y0 = maximum([y for (x,y) in board])+4
+        x0 = 2
+        p = Set([(x+x0,y+y0) for (x,y) in popfirst!(pp)])
+        while true
+            f = popfirst!(flow)
+            println("flow ", f)
+            if f == '>'
+                p1 = Set([(x+1,y) for (x,y) in p])
+                x1 = maximum(x for (x,_) in p1)
+                if isempty(intersect(board,p1)) && x1<=6
+                    p = p1
+                end
+            else 
+                p1 = Set([(x-1,y) for (x,y) in p])
+                x1 = minimum(x for (x,_) in p1)
+                if isempty(intersect(board,p1)) && 0<=x1
+                    p = p1
+                end
+            end
+            p1 = Set([(x,y-1) for (x,y) in p])
+            if isempty(intersect(p1, board)) 
+                println("sink")
+                p = p1 
+            else
+                println("stuck")
+                union!(board, p)
+                break
+            end    
+        end       
+    end
+    board
 end
 
 
 
-flow = read(open("input.txt"), String)
+flow = read(open("small.txt"), String)
 
 P = readPieces()
 
-tetris(flow, P, k)
+board = tetris(flow, P, 3)
+
+M = fill('.', 20,7)
+for (x,y) in board
+    if y>= 0 
+        M[20-y,x+1] = '#'
+    end
+end
+println(join([join(M[i,:]) for i in 1:20],"\n"))
 
