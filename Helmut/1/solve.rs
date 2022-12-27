f9195ed5967d0738c093ad99b7fdd521bf332a21
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-27 13:07:17 +0100

    sort only once

--- Helmut/1/solve.rs
+++ Helmut/1/solve.rs
@@ -1,13 +1,14 @@
 fn main() {
-    let _input = include_str!("input.txt");
+    let _ss = include_str!("input.txt");
 
-    let mut calories = _input
+    let mut calories = _ss
         .split("\n\n")
         .map(|cal| cal.lines().map(|x| x.parse::<i64>().unwrap()).sum())
         .collect::<Vec<i64>>();
 
     calories.sort();
+    calories.reverse();
 
-    println!("{}", calories.last().unwrap());
-    println!("{}", calories.iter().rev().take(3).sum::<i64>());
+    println!("{}", calories.first().unwrap());
+    println!("{}", calories.iter().take(3).sum::<i64>());
 }
