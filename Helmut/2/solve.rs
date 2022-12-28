6fbd30982d39ec3f7da3b8044df336f7a91557bd
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2022-12-28 11:18:45 +0100

    2 in rust

--- Helmut/2/solve.rs
+++ Helmut/2/solve.rs
@@ -1,14 +1,20 @@
-use itertools::Itertools;
-
-fn f(s : &str) -> i64 {
-    return 123;
+fn f(s: &str) -> i64 {
+    match s {
+        "A Y" => 6 + 2,
+        "B Z" => 6 + 3,
+        "C X" => 6 + 1,
+        "A X" => 3 + 1,
+        "B Y" => 3 + 2,
+        "C Z" => 3 + 3,
+        "A Z" => 0 + 3,
+        "B X" => 0 + 1,
+        "C Y" => 0 + 2,
+        _ => unreachable!(),
+    }
 }
 
 fn main() {
-    let ss : &str = include_str!("input.txt")
-    .lines()
-    .map(|x| {f(x)})
-    .sum();
+    let ss = include_str!("input.txt").lines().map(|s| f(s)).sum::<i64>();
 
-    println!("{:?}", ss);
+    println!("{}", ss);
 }
