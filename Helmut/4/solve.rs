2b91566557a4209915e5cbfbd135c40d07976948
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2023-01-01 18:48:29 +0100

    day4 in rust

--- Helmut/4/solve.rs
+++ Helmut/4/solve.rs
@@ -1,6 +1,41 @@
-fn main {
-    let lines = input_str!("input.txt").lines();
-    let i = 123;
+type Interval = (i64, i64);
 
-    pd
+fn contains(ab: Interval, cd: Interval) -> bool {
+    let (a, b) = ab;
+    let (c, d) = cd;
+    (a <= c) && ( d <= b ) || ( c <= a ) && ( b <= d )
+}
+
+fn overlaps1(ab: Interval, cd: Interval) -> bool {
+    let (a, b) = ab;
+    let (c, d) = cd;
+    (c <= a) && ( a <= d ) || ( c <= b ) && ( b <= d )
+}
+
+fn overlap(ab: Interval, cd: Interval) -> bool {
+    overlaps1(ab,cd) || overlaps1(cd, ab)
+}
+
+
+
+fn pint(a: &str, b: &str) -> Interval {
+    let aa = a.parse().unwrap();
+    let bb = b.parse().unwrap();
+    (aa, bb)
+}
+
+fn rdl(l: &str) -> (Interval, Interval) {
+    let (x, y) = l.split_once(",").unwrap();
+    let (a, b) = x.split_once("-").unwrap();
+    let (c, d) = y.split_once("-").unwrap();
+    (pint(a, b), pint(c, d))
+}
+
+fn main() {
+    let lines = include_str!("input.txt").lines();
+    let w = lines.map(rdl).collect::<Vec<(Interval, Interval)>>();
+    let r1 = w.clone().into_iter().filter(|(ab,cd)| {contains(*ab, *cd)}).count();
+    let r2 = w.into_iter().filter(|(ab,cd)| {overlap(*ab, *cd)}).count();
+    println!("{:?}", r1);
+    println!("{:?}", r2);
 }
