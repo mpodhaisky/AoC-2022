558dfda3b59a12eea5f5e556e6b069e1000c8988
Helmut Podhaisky <helmut.podhaisky@mathematik.uni-halle.de>
2023-01-02 08:05:39 +0100

    finish day5

--- Helmut/5/solve.rs
+++ Helmut/5/solve.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-fn mkStacks(a: &str) -> Vec<Vec<char>> {
+fn mk_stacks(a: &str) -> Vec<Vec<char>> {
     let v = a.lines().collect::<Vec<_>>();
     let n = (v.into_iter().last().unwrap().len() + 1) / 4;
     let mut stack: Vec<Vec<char>> = (0..n).map(|_| vec![]).collect();
@@ -16,19 +16,37 @@ fn mkStacks(a: &str) -> Vec<Vec<char>> {
     }
     stack
 }
-fn main() {
-    let (a, b) = include_str!("input.txt").split_once("\n\n").unwrap();
-    let mut stack = mkStacks(a);
+
+fn solve(stack: Vec<Vec<char>>, b: &str)
+{
+    let mut ss = stack.clone();
+    let mut s1 = stack.clone();
     for l in b.lines() {
         let ll = l.split(" ").collect::<Vec<_>>();
         let m = ll[1].parse::<usize>().unwrap();
         let i = ll[3].parse::<usize>().unwrap()-1;
         let j = ll[5].parse::<usize>().unwrap()-1;
+        let mut v:Vec<char> = vec![];
         for _ in 0..m {
-            let x = stack[i].pop().unwrap();
-            stack[j].push(x);
+            let x = ss[i].pop().unwrap();
+            ss[j].push(x);
+            let x = s1[i].pop().unwrap();
+            v.push(x);
+        }
+        for _ in 0..m {
+            let x: char = v.pop().unwrap();
+            s1[j].push(x);
         }
     }
-    let lsg = stack.into_iter().map(|s| {s.last().cloned().unwrap()}).collect::<String>();
-    println!("{}", lsg);
+    let part1 = ss.into_iter().map(|s| {s.last().cloned().unwrap()}).collect::<String>();
+    let part2 = s1.into_iter().map(|s| {s.last().cloned().unwrap()}).collect::<String>();
+    println!("{}", part1);
+    println!("{}", part2);
+
+}
+
+fn main() {
+    let (a, b) = include_str!("input.txt").split_once("\n\n").unwrap();
+    let stack = mk_stacks(a);
+    solve(stack, b);
 }
